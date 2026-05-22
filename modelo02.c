93eae5a1b1d1467f6e4dce0df0a1fe3e968b7ca6
vianei-kaiser <65637392+vianei-kaiser@users.noreply.github.com>
2026-05-22 12:05:09 +0000

    modified:   modelo02.c

--- modelo02.c
+++ modelo02.c
@@ -1,32 +1,39 @@
 #include <stdio.h>
- 
+
 int main() {
-    int matriz[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    int evenCount = 0, oddCount = 0;
- 
     // Contagem de elementos pares e ímpares
-    for (int i = 0; i < 3; i++) {      // Loop externo para as linhas
-        for (int j = 0; j < 3; j++) {  // Loop interno para as colunas
-            if (matriz[i][j] % 2 == 0) {
-                evenCount++; // Incrementa o contador de pares
+    int matrix[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    int even = 0, odd = 0;
+    int i, j;
+
+    // Loop externo para as linhas
+    for(i = 0; i < 3; i++) {
+        // Loop interno para as colunas
+        for(j = 0; j < 3; j++) {
+            if(matrix[i][j] % 2 == 0) {
+                // Incrementa o contador de pares
+                even++;
             } else {
-                oddCount++; // Incrementa o contador de ímpares
+                // Incrementa o contador de ímpares
+                odd++;
             }
         }
     }
- 
-    // Impressão dos resultados
 
-    // Impressão da matriz 
-    int i=0,j=0;
-    for (int i = 0; i < 3; i++) {      // Loop para imprimir cada linha
-        for (int j = 0; j < 3; j++) {  // Loop para imprimir cada coluna
-            printf("%3d ", matriz[i][j]); // %3d faz justificar a impressao dos numeros
+    // Impressão da matriz
+    printf("Matriz:\n");
+    // Loop para imprimir cada linha
+    for(i = 0; i < 3; i++) {
+        // Loop para imprimir cada coluna
+        for(j = 0; j < 3; j++) {
+            printf("%3d ", matrix[i][j]); // %3d faz justificar a impressao dos numeros
         }
         printf("\n");
+    }
 
-    printf("* Número de elementos pares  : %d\n", evenCount);
-    printf("* Número de elementos ímpares: %d\n", oddCount);
- 
+    // Impressão dos resultados
+    printf("Pares: %d\n", even);
+    printf("Ímpares: %d\n", odd);
+
     return 0;
 }
