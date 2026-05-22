558a1ebfb257315e75d452cc450879845d4acc61
vianei-kaiser <65637392+vianei-kaiser@users.noreply.github.com>
2026-05-22 11:41:56 +0000

    modified:   modelo02.c

--- modelo02.c
+++ modelo02.c
@@ -25,7 +25,7 @@ int main() {
         }
         printf("\n");
 
-    printf("Número de elementos pares: %d\n", evenCount);
+    printf("Número de elementos pares  : %d\n", evenCount);
     printf("Número de elementos ímpares: %d\n", oddCount);
  
     return 0;
