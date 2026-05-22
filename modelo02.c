a30dd0b2d0a5f087b503e8f84fdf83c19fe32d4e
vianei-kaiser <65637392+vianei-kaiser@users.noreply.github.com>
2026-05-22 11:40:59 +0000

    modified:   modelo02.c

--- modelo02.c
+++ modelo02.c
@@ -18,12 +18,13 @@ int main() {
     // Impressão dos resultados
 
     // Impressão da matriz 
+    int i=0,j=0;
     for (int i = 0; i < 3; i++) {      // Loop para imprimir cada linha
         for (int j = 0; j < 3; j++) {  // Loop para imprimir cada coluna
             printf("%3d ", matriz[i][j]); // %3d faz justificar a impressao dos numeros
         }
         printf("\n");
-        
+
     printf("Número de elementos pares: %d\n", evenCount);
     printf("Número de elementos ímpares: %d\n", oddCount);
  
